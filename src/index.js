f5ab6fd88ee6cc1794ee705009ce4a6adf77fc77
David Yem <david13200arles@hotmail.fr>
2021-07-23 21:57:50 +0200

    Sass carousel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 const imgDefault = document.getElementById('img-default')
 const alertMessage = document.getElementById('message-alert')
 const htmlCss = document.getElementById('html-css')
+const sass = document.getElementById('sass')
 
 /*Boutons*/
 const btnHtmlCss = document.getElementById('btn-html-css')
+const btnSass = document.getElementById('btn-sass')
 const btnAlertMessage = document.getElementById('btn-alert')
+
+function displayNone(){
+    imgDefault.style.display = 'none'
+    alertMessage.style.display = 'none'
+    htmlCss.style.display ='none'
+    sass.style.display = 'none'
+}
 
 btnAlertMessage.addEventListener('click',function(){/*bouton close du message alerte*/
     imgDefault.style.display = 'flex'
 })
-
 btnHtmlCss.addEventListener('click',function(e){/*bouton html5/css3*/
     e.preventDefault()
-    imgDefault.style.display = 'none'
-    alertMessage.style.display = 'none'
+    displayNone()
     htmlCss.style.display ='flex'
+})
+btnSass.addEventListener('click',function(e){
+    e.preventDefault()
+    displayNone()
+    sass.style.display = 'flex'
 })
 
